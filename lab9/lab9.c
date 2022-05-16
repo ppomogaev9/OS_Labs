80eb938dfa3b2ef949f8bf18f79bc889854cfb4b
ppomogaev9 <94314528+ppomogaev9@users.noreply.github.com>
2022-05-16 17:51:40 +0700

    Create lab9.c

---
+++ lab9/lab9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#define ERROR -1
+#define FORK_ERROR -1
+#define FORK_VAL_FOR_CHILD 0
+
+int main(int argc, const char** argv) {
+
+        pid_t pid = fork();
+        if (pid == FORK_ERROR) {
+                perror("error in fork");
+                return ERROR;
+        }
+
+        if (pid == FORK_VAL_FOR_CHILD) {
+                execlp("cat", "cat", "large_text.txt", NULL);
+                perror("Execlp didn't execute. Error");
+                return ERROR;
+        }
+
+        printf("\nParent proccess text!\n\n");
+
+        return 0;
+}
